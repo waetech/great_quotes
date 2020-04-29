6134fa31c260e3f922e9a6941a4ef0c92c02076e
Roxanne Reyes <waetechsolutions@gmail.com>
2020-04-29 16:29:36 -0400

    Added popular inspirational quotes

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,13 @@ class _QuoteListState extends State<QuoteList> {
 
   List<Quote> quotes = [
     Quote(author: 'Oscar Wilde', text: 'Be yourself; everyone else is already taken'),
-    Quote(author: 'Oscar Wilde', text: 'Be yourself; everyone else is already taken'),
-    Quote(author: 'Oscar Wilde', text: 'Be yourself; everyone else is already taken')
+    Quote(author: 'Henry Ford', text: 'If everyone is moving forward together, then success takes care of itself.'),
+    Quote(author: 'George Lucus', text: 'The technology keeps moving forward, which makes it easier for the artists to tell their stories and paint the pictures they want.'),
+    Quote(author: 'Confucious', text: 'Study the past if you would define the future'),
+    Quote(author: 'Albert Einstein', text: 'Two things are infinite: the universe and human stupidity; and Im not sure about the universe'),
+    Quote(author: 'Mark Twain', text: 'If you tell the truth, you dont have to remember anything')
+
+
 
   ];
   

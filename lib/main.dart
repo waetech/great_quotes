9f9a48355dd3c7fc55f95ed66d75e81a7b0acc21
Roxanne Reyes <waetechsolutions@gmail.com>
2020-04-29 22:52:27 -0400

    Created a delete function to remove a quote

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'quote.dart';
+import 'quote_card.dart';
 
 void main() => runApp(MaterialApp(
-  home: QuoteList(),
-));
+      home: QuoteList(),
+    ));
 
 class QuoteList extends StatefulWidget {
   @override
@@ -11,63 +12,39 @@ class QuoteList extends StatefulWidget {
 }
 
 class _QuoteListState extends State<QuoteList> {
-
   List<Quote> quotes = [
-    Quote(author: 'Oscar Wilde', text: 'Be yourself; everyone else is already taken'),
-    Quote(author: 'Henry Ford', text: 'If everyone is moving forward together, then success takes care of itself.'),
-    Quote(author: 'George Lucus', text: 'The technology keeps moving forward, which makes it easier for the artists to tell their stories and paint the pictures they want.'),
-    Quote(author: 'Confucious', text: 'Study the past if you would define the future'),
-    Quote(author: 'Albert Einstein', text: 'Two things are infinite: the universe and human stupidity; and Im not sure about the universe'),
-    Quote(author: 'Mark Twain', text: 'If you tell the truth, you dont have to remember anything')
-
-
-
+    Quote(
+        author: 'Oscar Wilde',
+        text: 'Be yourself; everyone else is already taken'),
+    Quote(
+        author: 'Henry Ford',
+        text:
+            'If everyone is moving forward together, then success takes care of itself.'),
+    Quote(author: 'George Lucus', text: 'May the force be with you.')
   ];
-  
-  Widget quoteTemplate(quote){
-    return Card(
-      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Text(
-              quote.text,
-              style: TextStyle(
-                fontSize: 18.0,
-                color: Colors.grey[600],
-              ),
-            ),
-            SizedBox(height: 6.0),
-            Text(
-              quote.author,
-              style: TextStyle(
-                fontSize: 14.0,
-                color: Colors.grey[800],
-              ),
-            ),
-          ],
-        ),
-      ),
 
-     );
-  }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-        title: Text('Awesome Quotes'),
-        centerTitle: true,
-        backgroundColor: Colors.redAccent,
-      ),
-      body: Column(
-        children: quotes.map((quote) => quoteTemplate(quote)).toList(),
-      )
+        backgroundColor: Colors.grey[200],
+        appBar: AppBar(
+          title: Text('Awesome Quotes'),
+          centerTitle: true,
+          backgroundColor: Colors.redAccent,
+        ),
+        body: Column(
+            children: quotes.map((quote) => QuoteCard(
+              quote: quote,
+              delete: () {
+                setState(() {
+                  quotes.remove(quote);
+                });
+              }
+            )).toList(),
+         )
     );
   }
 }
-
 
